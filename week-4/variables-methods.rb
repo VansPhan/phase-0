348bf54a057621a80f8cd918a071b9d7e90278d1
Van Phan <vanphan514@gmail.com>
2015-10-28 20:05:37 -0400

    Added finished 4.3 q&a to variables-methods.rb

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -14,3 +14,33 @@ puts "What is your favorite number?"
 favorite_number = gets.chomp.to_i
 puts "Not trying to one up you or anything.."
 puts "But I believe #{favorite_number + 1} is better than #{favorite_number} though.."
+
+#ADDRESS LINK
+#https://github.com/VansPhan/phase-0/blob/master/week-4/address/my_solution.rb
+
+#MATH LINK
+#https://github.com/VansPhan/phase-0/blob/master/week-4/math/my_solution.rb
+
+# How do you define a local variable?
+# Have a valid variable name and make it equal to an object.
+
+# How do you define a method?
+# You define a method by wrapping your code with "def" followed by a method name of your choice
+# and then finally putting "end" at where your code ends.
+
+# What is the difference between a local variable and a method?
+# A local variable can only be used in its scope and is on a reference of an object. A
+# method can be used globally and consist of many lines of code that accomplish a
+# specific task.
+
+# How do you run a ruby program from the command line?
+# You type "ruby" followed by the file name of your ruby file.
+
+# How do you run an RSpec file from the command line?
+# You type "rspec" followed by the file name of your test.
+
+# What was confusing about this material? What made sense?
+# I understood the logic behind implementing the material but had trouble with
+# syntax and code format. I understand that there is no perfect way to write code, 
+# but I can improve my naming with variables and methods to better express my
+# code clearly.
